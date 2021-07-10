ce8336aa39490a43e392c3666ff107e1bdcc7c80
kim dong joo <ccomekdj@gmail.com>
2021-07-10 21:13:40 +0900

    create ControlButton widget inf control bar

--- lib/page/control_bar.dart
+++ lib/page/control_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:paint_board_test/res/theme_data.dart';
 
 class ControlBar extends StatefulWidget {
   const ControlBar({Key key}) : super(key: key);
@@ -14,6 +15,27 @@ class _ControlBarState extends State<ControlBar> {
       width: double.infinity,
       height: 80,
       color: Colors.grey[300],
+      child: Row(
+        children: [
+       
+        ],
+      ),
+    );
+  }
+
+  controlButton({String title}){
+    if(title==null) title = "";
+    return Container(
+      decoration: boxTheme.basicOutlineGreyBox,
+      height: 60,
+      width: 60,
+      child: Center(
+        child: Text(
+          title
+        ),
+      ),
     );
   }
 }
+
+
