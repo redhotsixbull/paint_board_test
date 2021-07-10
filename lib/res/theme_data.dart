ebf9a1a67470c94d5bd6045c3dced8e2997ffe2f
kim dong joo <ccomekdj@gmail.com>
2021-07-10 21:13:16 +0900

    Create theme_data.dart

---
+++ lib/res/theme_data.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BoxTheme {
+  final BoxDecoration basicOutlineGreyBox = BoxDecoration(
+      color: Colors.grey[400],
+      borderRadius: BorderRadius.circular(16));
+}
+
+class TextTheme {}
+
+final boxTheme = BoxTheme();
+final textTheme = TextTheme();
